fdb782eca1ee1d07b955aacdd077621bf1b3481d
sedron <jfabianabadd@gmail.com>
2024-01-01 13:36:09 -0500

    Created Category Service and Create Product form

---
+++ Services/CategoryService.cs
@@ -0,0 +1,31 @@
+using System.Text.Json;
+using WASM_Platzi.Models;
+namespace WASM_Platzi.Services;
+
+public class CategoryService : ICategoryService
+{
+      private readonly HttpClient client;
+        private readonly JsonSerializerOptions options;
+        public CategoryService(HttpClient client)
+        {
+            this.client = client;
+            options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        }
+
+        public async Task<List<Category>> Get()
+        {
+            var response = await client.GetAsync("v1/categories");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+
+            return JsonSerializer.Deserialize<List<Category>>(content, options);
+        }
+}
+
+public interface ICategoryService
+{
+    Task<List<Category>> Get();
+}
